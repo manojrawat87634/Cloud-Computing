ecce3bd2c87f4308fae6bd0e035625a581561a5c
unknown <manojrawat87634@gmail.com>
2026-09-16 11:55:04 +0530

    loops in bash scripting

--- 01_bash_scripting/01_shell_script/03_loops/02_while/04_ans/index.sh
+++ 01_bash_scripting/01_shell_script/03_loops/02_while/04_ans/index.sh
@@ -1,10 +1,8 @@
-
 read -p "Enter number : " num
 i=1
-while [ $i -le $num ] ; do
-    if [ $((i % 2 )) -eq 0 ]; then
+while [ $i -le $num ] ;do
+    if [ $((i % 2 )) -ne 0 ]; then
         echo $i
     fi
     i=$(( i+1 ))
-
     done
--- 01_bash_scripting/01_shell_script/03_loops/02_while/index.sh
+++ 01_bash_scripting/01_shell_script/03_loops/02_while/index.sh
@@ -1,8 +1,8 @@
 read -p "Enter number : " num
-i=1
-while [ $i -le $num ] ; do
-    if [ $((i % 2 )) -eq 0 ]; then
-        echo $i
-    fi
-    i=$(( i + 1 ))
-done
+res=1
+while [ $num -gt 1 ] ;do
+    res=$((res * num))
+    num=$(( num - 1))
+    done
+
+echo $res
